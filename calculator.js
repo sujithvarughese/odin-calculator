56c47dd2159b2498b80f13726105a5f83a51b44f
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2022-11-17 19:54:02 -0500

    First Commit

---
+++ calculator.js
@@ -0,0 +1,10 @@
+
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide =(x, y) => x / y;
+
+    // take two numbers and call one of above functions
+function operate(x, y) {
+
+}
