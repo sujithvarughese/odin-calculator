c1c6a4dfd95ce60d4f67c411a578b1f095a6052b
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-06-23 02:09:02 -0400

    cleaned up logic, need to fix bug where component does not re-render if user chooses same operators consecutively

--- react/src/Calculator.jsx
+++ react/src/Calculator.jsx
@@ -9,35 +9,60 @@ const Calculator = () => {
 	// for current number being input
 	const [value, setValue] = useState('')
 
-	useEffect(()=> {
-		setValue('0')
-	}, [result])
+	// store the previous and current operators
+	const [operation, setOperation] = useState(null)
+	const [prevOperation, setPrevOperation] = useState(null)
 
 	const add = () => {
 		setResult(result + Number(value))
 	}
-	const subtract = (value) => {
+	const subtract = () => {
 		setResult(result - Number(value))
 	}
-	const multiply = (value) => {
+	const multiply = () => {
 		setResult(result * Number(value))
 	}
-	const divide = (value) => {
+	const divide = () => {
 		if (value === '0') {
 			setResult("Division by zero is not allowed")
 		}
 		setResult(result / Number(value))
 	}
 
-
 	const inputDigit = (digit) => {
 		setValue(value + digit)
 	}
-	console.log(result);
+
+	const equals = () => {
+
+	}
+	// FIXME re-render is not triggered if operator is the same for a 2nd time in a row
+	useEffect(() => {
+		console.log(prevOperation);
+		console.log(operation);
+		if (prevOperation === 'add') {
+			add()
+		}
+		if (prevOperation === 'subtract') {
+			subtract()
+		}
+		if (prevOperation === 'multiply') {
+			multiply()
+		}
+		if (prevOperation === 'divide') {
+			divide()
+		}
+		setValue('')
+
+		setPrevOperation(operation)
+	}, [operation])
 
 	return (
 		<div>
 			<div className="page-grid">
+				<div className="screen">
+					{value}
+				</div>
 				<div className="buttons-grid">
 					<div className="numbers-grid">
 						<button id="7" onClick={()=>inputDigit('7')}>7</button>
@@ -49,15 +74,20 @@ const Calculator = () => {
 						<button id="1" onClick={()=>inputDigit('1')}>1</button>
 						<button id="2" onClick={()=>inputDigit('2')}>2</button>
 						<button id="3" onClick={()=>inputDigit('3')}>3</button>
-						<button id="C/E" onClick={()=>setValue('0')}>CLEAR</button>
+						<button id="decimal" onClick={()=>inputDigit('.')}>.</button>
 						<button id="0"  onClick={()=>inputDigit('0')}>0</button>
 						<button id="neg">+/-</button>
 					</div>
 					<div className="operations-grid">
-						<button onClick={divide} id="div">/</button>
-						<button onClick={multiply} id="multiply">x</button>
-						<button onClick={subtract} id="sub">-</button>
-						<button onClick={add} id="add">+</button>
+						<button onClick={()=>setOperation('divide')} id="div">/</button>
+						<button onClick={()=>setOperation('multiply')} id="multiply">x</button>
+						<button onClick={()=>setOperation('subtract')} id="sub">-</button>
+						<button onClick={()=>setOperation('add')} id="add">+</button>
+					</div>
+					<div className="function-grid">
+						<button id="clear-all" onClick={()=>setResult(0)}>C/E</button>
+						<button id="clear" onClick={()=>setValue('')}>CLEAR</button>
+						<button id="equals" onClick={equals}>=</button>
 					</div>
 				</div>
 			</div>
